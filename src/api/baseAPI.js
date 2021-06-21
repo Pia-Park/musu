d396657133d10890c065d7089fa4e725959e1f9d
Daisugi <volble124@gmail.com>
2021-06-21 01:21:21 -0700

    modified base API

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -4,14 +4,38 @@ import axios from "axios";
  * Get products data from BASE
  * @return {[object]} the list of retrieved BASE product data
  */
-export async function getBaseProducts(baseToken) {
+export async function getBaseProducts(baseRefreshToken) {
+  const accessToken = await getAccessToken(baseRefreshToken);
+
   return await axios.get(
     process.env.REACT_APP_BASE_BASE_ENDPOINT + "/1/items",
     {
+      headers: {
+        Authorization: accessToken,
+      },
       params: {
         visible: "1",
         order: "created",
         sort: "desc",
+      },
+    }
+  );
+}
+
+/**
+ * Get products data from BASE
+ * @return {[object]} the list of retrieved BASE product data
+ */
+async function getAccessToken(baseRefreshToken) {
+  return await axios.get(
+    process.env.REACT_APP_BASE_BASE_ENDPOINT + "/1/items",
+    {
+      params: {
+        grant_type: "refresh_token",
+        client_id: process.env.REACT_APP_BASE_CLIENT_ID,
+        client_secret: process.env.REACT_APP_BASE_CLIENT_SECRET,
+        refresh_token: baseRefreshToken,
+        redirect_uri: process.env.REACT_APP_BASE_REDIRECT_URL,
       },
     }
   );
