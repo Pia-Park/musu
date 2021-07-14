39f63d2deeebaebb53fa2da1e08be0f223fd756a
Sh11ntar0 <sh11ntar0h@gmail.com>
2021-07-14 12:20:11 -0700

    fixed about.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,24 +4,26 @@ import "../App.css"
 
 function About() {
 	return (
-		<div className="about-container">
+		<section className="about-container">
 			<h2 className="title">About</h2>
 			<div className="about-detail">
-				<img src={`${process.env.PUBLIC_URL}/img/logo-musu.png`} alt="logo2" className="about-logo"/>
-				<p>『むす』について<br />
-					<br />
-				日本には「結び」という祈りや願いが込められた<br />
-				神聖な言葉があります。<br />
-				「結び」は「むすひ（産霊）」という言葉から生まれ、<br />
-				「むす」には「新しいものを生み出す」<br />
-				そして「ご縁を結び繋がる」<br />
-				という意味を持ちます。<br />
-				「むす」を通じて、本来の自分と繋がり魂が輝き<br />
-				自分らしく生きるために、あなたの魂が光輝き心清らかに<br />
-				なりますよう願いをこめております。
-				</p>
+				<img src={`${process.env.PUBLIC_URL}/img/logo-musu.png`} alt="logo2" className="about-logo" />
+				<div className="about-article">
+					<h3 className="contents-title">『むす』について</h3>
+					<p className="contents">
+					日本には「結び」という祈りや願いが込められた<br />
+					神聖な言葉があります。<br />
+					「結び」は「むすひ（産霊）」という言葉から生まれ、<br />
+					「むす」には「新しいものを生み出す」<br />
+					そして「ご縁を結び繋がる」<br />
+					という意味を持ちます。<br />
+					「むす」を通じて、本来の自分と繋がり魂が輝き<br />
+					自分らしく生きるために、あなたの魂が光輝き心清らかに<br />
+					なりますよう願いをこめております。
+					</p>
+				</div>
 			</div>
-		</div>
+		</section>
 
 
 	)
