17911fdb07cb91faca6a0101468a3cfd0a79b5c4
Sh11ntar0 <sh11ntar0h@gmail.com>
2021-07-21 14:15:21 -0700

    modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect, createContext } from "react";
 
 import "./App.css";
 import { setAccessToken } from "./api/commonAPI";
-import Instagram from "./components/Instagram";
+// import Instagram from "./components/Instagram";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
