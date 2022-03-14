fac9d4c65bb1d5ea3533dc621fa283dabc436b63
Pia Park <inae1park@gmail.com>
2022-03-13 21:05:19 -0700

    changed css and logo

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Footer.css"
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+function Footer() {
+	return (
+		<section className="footer-wrapper">
+			<div className="footer-container">
+					<AnchorLink href="#home" className="navbar-logo" ><img src={`${process.env.PUBLIC_URL}/img/soso_logo_ivory.png`} alt="soso Logo" className='logo-img'/>
+					</AnchorLink>				
+					<ul className="footer-links">
+					<li className="footer-link">Home</li>
+					<li className="footer-link">About</li>
+					<li className="footer-link">Shop</li>
+					<li className="footer-link">Contact</li>
+				</ul>
+			</div>
+			<h3 className="copywrite">copywrite@footer information part desu</h3>
+
+
+		</section>
+	)
+}
+
+export default Footer
---
+++ src/containers/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+function Navbar() {
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+
+
+	return (
+		<>
+			<nav className="navbar">
+
+					<div className="navbar-container">
+						<AnchorLink href="#home" className="navbar-logo" ><img src={`${process.env.PUBLIC_URL}/img/soso_logo_gray.png`} alt="soso Logo" className='logo'/>
+						</AnchorLink>
+						<div className="menu-icon" onClick={handleClick}>
+							<i className={click ? "fas fa-times" : "fas fa-bars"} />
+						</div>
+						<ul className={click ? "nav-menu active" : "nav-menu"}>
+							<li className="nav-item">
+								<AnchorLink href="#home" onClick={closeMobileMenu} className="nav-links">
+									Home
+								</AnchorLink>
+							</li>
+							<li className="nav-item">
+								<AnchorLink href="#about" onClick={closeMobileMenu} className="nav-links">
+									About
+								</AnchorLink>
+							</li>
+							<li className="nav-item">
+								<AnchorLink href="#shop" onClick={closeMobileMenu} className="nav-links">
+									Online Shop
+								</AnchorLink>
+							</li>
+							<li className="nav-item">
+								<AnchorLink href="#contact" onClick={closeMobileMenu} className="nav-links">
+									Contact
+								</AnchorLink>
+							</li>
+						</ul>
+					</div>
+			</nav>
+		</>
+	)
+}
+
+export default Navbar
